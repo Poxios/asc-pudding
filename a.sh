33ed03901ad1791301f70c185abe777e5c4fcdff
Poxios <poxios0310@gmail.com>
2023-11-17 15:06:14 +0900

    updated

--- a.sh
+++ a.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://github.com/Poxios/asc-pudding/raw/master/xaa
-wget https://github.com/Poxios/asc-pudding/raw/master/xab
-wget https://github.com/Poxios/asc-pudding/raw/master/xac
+wget -c https://github.com/Poxios/asc-pudding/raw/master/xaa
+wget -c https://github.com/Poxios/asc-pudding/raw/master/xab
+wget -c https://github.com/Poxios/asc-pudding/raw/master/xac
 
 cat xaa xab xac > a.tar
 tar -xvf a.tar
